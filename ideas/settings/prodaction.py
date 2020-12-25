ebd9941de47174d58f71b5f09c0ae45f4dfe3078
Bakdauley <bakosh21345@gmail.com>
2020-12-25 11:27:16 +0600

    sadv

--- ideas/settings/prodaction.py
+++ ideas/settings/prodaction.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django-ideas.herokuapp.com/']
+ALLOWED_HOSTS = ['django-ideas.herokuapp.com']
 
 
 # Application definition
