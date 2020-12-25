6dafd6265cf57114195d8b1cd1ff8b8cbf8656b3
Bakdauley <bakosh21345@gmail.com>
2020-12-25 11:06:39 +0600

    s

--- ideas/settings/prodaction.py
+++ ideas/settings/prodaction.py
@@ -136,10 +136,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'ideas/static'),
+)
 
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = '465'
